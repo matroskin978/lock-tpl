b0208ea82961f1ec47aa9310f11d8ad44bb01aa5
Админ <matroskin8@i.ua>
2024-09-28 13:09:51 +0300

    lesson 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 
-    console.log('123');
+    $('#search-form-btn').on('click', function (e) {
+        e.preventDefault();
+        let form = $(this).parent();
+        let inputSearch = form.find('.form-control');
+        inputSearch.toggleClass('show').focus();
+        if (inputSearch.val()) {
+            form.submit();
+        }
+    });
 
 });
