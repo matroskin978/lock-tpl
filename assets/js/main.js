346d8aecaf62398c6ca91844e54479582d8d268e
Админ <matroskin8@i.ua>
2025-01-06 11:54:15 +0200

    lesson 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,50 @@ $(function () {
         }
     });
 
+    const mainOlwProducts = $('.owl-carousel').owlCarousel({
+        // loop: true,
+        stagePadding: 30,
+        margin: 20,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            700: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    const mainOlwProductsNextBtn = $('.next-btn');
+    const mainOlwProductsPrevBtn = $('.prev-btn');
+
+    mainOlwProductsNextBtn.click(function () {
+        mainOlwProducts.trigger("next.owl.carousel");
+    });
+    mainOlwProductsPrevBtn.click(function () {
+        mainOlwProducts.trigger("prev.owl.carousel");
+    });
+
+    mainOlwProductsPrevBtn.addClass("disabled");
+    mainOlwProducts.on("translated.owl.carousel", function (event) {
+        if ($(".main-our-products .owl-prev").hasClass("disabled")) {
+            mainOlwProductsPrevBtn.addClass("disabled");
+        } else {
+            mainOlwProductsPrevBtn.removeClass("disabled");
+        }
+        if ($(".main-our-products .owl-next").hasClass("disabled")) {
+            mainOlwProductsNextBtn.addClass("disabled");
+        } else {
+            mainOlwProductsNextBtn.removeClass("disabled");
+        }
+    });
+
 });
