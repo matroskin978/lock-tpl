e5a3a23484cf7d09d7e715b185eea24e42745a6a
Админ <matroskin8@i.ua>
2025-06-07 11:57:32 +0300

    lesson 19

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,3 +156,58 @@ $(function () {
     });
 
 });
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+        'max-age': -1
+    })
+}
+
+// Пример использования:
+// setCookie('user', 'John', { 'max-age': 10 });
+// console.log(getCookie('user'));
+const offcanvasCookie = new bootstrap.Offcanvas('#offcanvasCookie');
+
+if (!getCookie('allowCookie')) {
+    setTimeout(() => {
+        offcanvasCookie.show();
+    }, 3000);
+}
+
+document.querySelector('#allow-cookie').addEventListener('click', (e) => {
+    e.preventDefault();
+    setCookie('allowCookie', 1, { 'max-age': 3600 * 24 * 365 });
+    offcanvasCookie.hide();
+});
